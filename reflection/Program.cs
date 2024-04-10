7df243303bbc4335df140016786f17fab8226ff0
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-04-10 07:14:57 -0300

    Initial commit

---
+++ reflection/Program.cs
@@ -0,0 +1,5 @@
+﻿using reflection.infra;
+
+var prefixes = new string[] { "http://localhost:5341/" };
+var webApplication = new WebApplication(prefixes);
+webApplication.Open();
---
+++ reflection/infra/WebApplication.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Text;
+
+namespace reflection.infra
+{
+    public class WebApplication
+    {
+        private readonly string[] _prefixes;
+        public WebApplication(string [] prefixes)
+        {
+            if (prefixes is null)
+                throw new ArgumentNullException(nameof(prefixes));
+
+            _prefixes = prefixes;
+        }
+        public void Open()
+        {
+            var httpListener = new HttpListener();
+
+            foreach (var prefix in _prefixes)
+            {
+                httpListener.Prefixes.Add(prefix);
+            }
+
+            httpListener.Start();
+
+            var context = httpListener.GetContext();
+            var request = context.Request;
+            var response = context.Response;
+
+            var contentResponse = "Hello World!";
+            var contentResponseBytes = Encoding.UTF8.GetBytes(contentResponse);
+
+            response.ContentType = "text/html; charset=utf-8";
+            response.StatusCode = 200;
+            response.ContentLength64 = contentResponseBytes.Length;
+
+            response.OutputStream.Write(contentResponseBytes, 0, contentResponseBytes.Length);
+
+            response.OutputStream.Close();
+
+            httpListener.Stop();
+        }
+    }
+}
